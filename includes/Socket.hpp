39ce8f8674a48e3c247679def376ddcc054c13e0
Hugo SOSZYNSKI <hugo.soszynski@epitech.eu>
2017-02-15 20:30:33 +0100

    Added .gitignore and CMakeList.txt

---
+++ includes/Socket.hpp
@@ -0,0 +1,10 @@
+/*
+** Socket.hpp for CPP_Simple_Networking_Library in /home/soszyn_h/Documents/CPP_Simple_Networking_Library/includes/Socket.hpp
+**
+** Made by Hugo SOSZYNSKI
+** Login   <hugo.soszynski@epitech.eu>
+**
+** Started on  Wed Feb 15 19:29:50 2017 Hugo SOSZYNSKI
+** Last update Wed Feb 15 19:29:50 2017 Hugo SOSZYNSKI
+*/
+
