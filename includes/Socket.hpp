f1a2129c30b8f3f26eda2d6e7bd158e40ef3231a
Maxime Pillon <maxime.pillon@epitech.eu>
2017-02-28 19:34:38 +0100

    Socket starrting

--- includes/Socket.hpp
+++ includes/Socket.hpp
@@ -8,3 +8,34 @@
 ** Last update Wed Feb 15 19:29:50 2017 Hugo SOSZYNSKI
 */
 
+#ifndef SOCKET_HPP
+# define SOCKET_HPP
+
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+class Socket {
+protected:
+  int			_domain;
+  int			_type;
+  int			_protocol;
+  int			_socket;
+  struct sockaddr_in	_destination;
+
+public:
+  Socket(int domain, int type, int protocol);
+  Socket(Socket const& other);
+  ~Socket();
+
+  Socket& operator=(Socket const& other);
+  void SocketPair(sa_family_t domain, uint16_t portNum, std::string addr);
+  void CreateConnection();
+};
+
+#endif /* !SOCKET_HPP */
--- sources/Socket.cpp
+++ sources/Socket.cpp
@@ -8,3 +8,45 @@
 ** Last update Wed Feb 15 19:29:53 2017 Hugo SOSZYNSKI
 */
 
+#include "Socket.hpp"
+
+Socket::Socket(int domain, int type, int protocol)
+  : _domain(domain), _type(type), _protocol(protocol)
+{
+  this->_socket = socket(domain, type, protocol);
+}
+
+Socket::Socket(Socket const &other)
+{
+  this->_domain = other._domain;
+  this->_protocol = other._protocol;
+  this->_socket = other._socket;
+  this->_type = other._type;
+}
+
+Socket::~Socket()
+{
+  close(_socket);
+}
+
+Socket& Socket::operator=(Socket const &other)
+{
+  this->_domain = other._domain;
+  this->_protocol = other._protocol;
+  this->_socket = other._socket;
+  this->_type = other._type;
+}
+
+void Socket::SocketPair(sa_family_t domain, uint16_t portNum, std::string addr)
+{
+  // check error
+  memset(&this->_destination, 0, sizeof(struct sockaddr_in));
+  this->_destination.sin_family = domain;
+  this->_destination.sin_port = htons(portNum); // check error
+  this->_destination.sin_addr.s_addr = inet_addr(addr.c_str()); // check error
+}
+
+void Socket::CreateConnection()
+{
+ connect(this->_socket, (struct sockaddr*)&this->_destination, sizeof(struct sockaddr_in)); // check error
+}
