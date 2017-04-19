c671be8f1883b058eba8ba93d333ab7580256c9d
Hugo SOSZYNSKI <hugo.soszynski@epitech.eu>
2017-04-19 20:48:04 +0200

    Good progress on the main Socket class with ctor & dtor and some needed setters + connet method

--- includes/Socket.hpp
+++ includes/Socket.hpp
@@ -11,38 +11,100 @@
 #ifndef SOCKET_HPP
 # define SOCKET_HPP
 
-# include <stdlib.h>
-# include <string.h>
+# include <cstdlib>
+# include <cstring>
 # include <string>
 # include <unistd.h>
 # include <arpa/inet.h>
 # include <netinet/in.h>
 # include <sys/socket.h>
-# include <sys/types.h>
+# include <ctype.h>
+# include <sys/un.h>
 # include "SocketException.hpp"
 
-class Socket
+/// \namespace socket
+namespace socket
 {
-protected:
-  int			_domain;
-  int			_type;
-  int			_protocol;
-  int			_socket;
-  struct sockaddr_in	_destination;
+  /// \class Socket
+  class Socket
+  {
+  protected:
+    int _domain; //!< Specifies a communication domain, this selects the protocol family which will be used for communication.
+    int _type; //!< The socket type which specifies the communication semantics.
+    int _protocol; //!< Protocol to be used with the socket.
+    int _socket; //!< The local socket file descriptor.
+    int _clientSocket; //!< The client socket (not initialized by default).
+    struct sockaddr *_address; //!< The local socket's sockaddr structure.
+    struct sockaddr *_client; //!< The client socket's sockaddr structure.
+    socklen_t _addressLen; //!< The local sockaddr length.
+    socklen_t _clientLen; //!< The client's sockaddr length.
 
-public:
-  Socket(int domain, int type, int protocol);
-  ~Socket();
+  public:
+    /// \brief Constructor for Socket objects.
+    /// \param domain Specifies a communication domain, this selects the protocol family which will be used for communication.
+    ///               These families are defined in <sys/socket.h>.
+    /// \param type The socket has the indicated type, which specifies the communication semantics. Could be AF_INET or AF_UNIX.
+    /// \param protocol The protocol specifies a particular protocol to be used with the socket.
+    ///                 Normally only a single protocol exists to support a particular socket type within a given protocol family, in which case protocol can be specified as 0.
+    Socket(int domain, int type, int protocol);
 
-public:
-  void Pair(sa_family_t domain, uint16_t portNum, std::string addr);
-  void Connect();
+    /// \brief Destructor for Socket objects. Close the socket.
+    ~Socket();
 
-private:
-  Socket(Socket const& other) = delete;
-  Socket(Socket const&& other) = delete;
-  Socket& operator=(Socket const& other) = delete;
-  Socket& operator=(Socket const&& other) = delete;
-};
+  public:
+    /// \brief Set the content of the structure used for AF_INET socket.
+    ///        NEVER use this method if you use a AF_UNIX socket.
+    /// \param sinPort Port to connect to.
+    /// \param sinAddr IPv4 address in binary form.
+    void setInAddress(unsigned short sinPort, struct in_addr sinAddr);
+
+    /// \brief Set the content of the structure used for AF_UNIX socket.
+    ///        NEVER use this method if you use a AF_INET socket.
+    /// \param pathname The pathname for the socket (max 108 char, else will be truncated).
+    void setUnAddress(std::string const& pathname);
+
+    /// \brief Close the client socket. If the socket was not open, undefined behaviour may occur.
+    void closeClientSocket();
+
+  public:
+    /// \brief Attempt to make a connection on a connection-mode socket or to set or reset the peer address of a connectionless-mode socket.
+    /// \return Upon successful completion, this method shall return 0; otherwise, −1 shall be returned.
+    int Connect();
+
+    /// \brief Assign a local socket address to the socket.
+    /// \return Upon successful completion, the method shall return 0; otherwise, −1 shall be returned.
+    int Bind();
+
+    /// \brief Mark a connection-mode socket as accepting connections.
+    /// \param backlog The maximum number of connection to the socket (MUST be LESS than SOMAXCONN defined in <sys/socket.h>)
+    /// \return Upon successful completion, the method shall return 0; otherwise, −1 shall be returned.
+    int Listen(int backlog);
+
+    /// \brief Extract the first connection on the queue of pending connections and allocate a new file descriptor for that socket (here the clientSocket of the class).
+    /// \return Upon successful completion, the method shall return 0; otherwise, −1 shall be returned.
+    int Accept();
+
+    /// \brief Initiate transmission of a message from the socket to its peer.
+    /// \param buf Point to the buffer containing the message to send.
+    /// \param length The length of the message in bytes.
+    /// \return Upon successful completion, return the number of bytes sent, otherwise return -1.
+    ssize_t Send(void const* buf, size_t length);
+
+    /// \brief
+    /// \param buf Points to the buffer where the message should be stored.
+    /// \param maxLen The length of the buffer pointed by the buf argument.
+    /// \return The length of the received message of successful completion, otherwise return -1.
+    ssize_t Recv(void *buf, size_t maxLen);
+
+  private:
+    Socket(Socket const &other) = delete;
+
+    Socket(Socket const &&other) = delete;
+
+    Socket &operator=(Socket const &other) = delete;
+
+    Socket &operator=(Socket const &&other) = delete;
+  };
+}
 
 #endif /* !SOCKET_HPP */
--- sources/Socket.cpp
+++ sources/Socket.cpp
@@ -11,21 +11,81 @@
 #include <errno.h>
 #include "Socket.hpp"
 
-Socket::Socket(int domain, int type, int protocol)
-  : _domain(domain), _type(type), _protocol(protocol)
+namespace socket
 {
-  this->_socket = socket(domain, type, protocol);
-  if (this->_socket == -1)
-    throw SocketException(strerror(errno));
-}
 
-Socket::~Socket()
-{}
+  Socket::Socket(int domain, int type, int protocol)
+    : _domain(domain), _type(type), _protocol(protocol), _socket(-1),
+      _clientSocket(-1), _address(NULL), _client(NULL),
+      _addressLen(0), _clientLen(0)
+  {
+    if (type == AF_INET)
+    {
+      this->_addressLen = sizeof(sockaddr_in);
+      this->_clientLen = sizeof(sockaddr_in);
+      this->_address = reinterpret_cast<sockaddr*>(new sockaddr_in);
+      this->_client = reinterpret_cast<sockaddr*>(new sockaddr_in);
+    }
+    else if (type == AF_UNIX)
+    {
+      this->_addressLen = sizeof(sockaddr_un);
+      this->_clientLen = sizeof(sockaddr_un);
+      this->_address = reinterpret_cast<sockaddr*>(new sockaddr_un);
+      this->_client = reinterpret_cast<sockaddr*>(new sockaddr_un);
+    }
+    else
+      throw SocketException("ERROR: Socket type not implemented");
+    std::memset(this->_address, 0, this->_addressLen);
+    std::memset(this->_client, 0, this->_clientLen);
+    this->_socket = socket(domain, type, protocol);
+    if (this->_socket == -1)
+      throw SocketException(strerror(errno));
+  }
 
-void Socket::Connect()
-{
- if (connect(this->_socket,
-	     (struct sockaddr*)&this->_destination,
-	     sizeof(struct sockaddr_in)) == -1)
-   throw SocketException(strerror(errno));
+  Socket::~Socket()
+  {
+    if (this->_type == AF_INET)
+    {
+      delete reinterpret_cast<sockaddr_in*>(this->_address);
+      delete reinterpret_cast<sockaddr_in*>(this->_client);
+    }
+    else if (this->_type == AF_UNIX)
+    {
+      delete reinterpret_cast<sockaddr_un*>(this->_address);
+      delete reinterpret_cast<sockaddr_un*>(this->_client);
+    }
+    close(this->_socket);
+  }
+
+  void Socket::setInAddress(unsigned short sinPort, struct in_addr sinAddr)
+  {
+    sockaddr_in* addr;
+
+    addr = reinterpret_cast<sockaddr_in*>(this->_address);
+    addr->sin_family = AF_INET;
+    addr->sin_port = sinPort;
+    addr->sin_addr = sinAddr;
+  }
+
+  void Socket::setUnAddress(std::string const &pathname)
+  {
+    sockaddr_un* addr;
+
+    addr = reinterpret_cast<sockaddr_un*>(this->_address);
+    addr->sun_family = AF_UNIX;
+    std::strncpy(addr->sun_path, pathname.c_str(), 108); // addr->sun_path[108] (RTFM)
+  }
+
+  void Socket::closeClientSocket()
+  {
+    close(this->_clientSocket);
+    std::memset(this->_client, 0, this->_clientLen);
+  }
+
+  int Socket::Connect()
+  {
+    return connect(this->_socket,
+		   this->_address,
+		   this->_addressLen);
+  }
 }
